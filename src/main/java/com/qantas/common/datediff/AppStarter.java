4d70d4e78dd62d5c8df5cca7afb7e362fbb7772d
Sureshraja <sureshraja.sun@gmail.com>
2023-03-18 10:42:37 +1100

    File name changes

--- src/main/java/com/qantas/common/datediff/AppStarter.java
+++ src/main/java/com/qantas/common/datediff/AppStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AppStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run( AppStarter.class, args);
 	}
 
 }
